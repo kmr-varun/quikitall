f16fd1c628c662832e67e0ef5005e2b06d9c7409
Varun Kumar <kmr11varun@gmail.com>
2024-09-12 11:59:00 +0530

    upload

--- main.js
+++ main.js
@@ -12,82 +12,87 @@ function raf(time) {
 
 requestAnimationFrame(raf)
 
-let tl4 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".part-4",
-        start: "top top",
-        end: "300% 50%",
-        pin: true,
-        scrub: 1,
-    },
-});
+// let tl4 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".part-4",
+//         start: "top top",
+//         end: "300% 50%",
+//         pin: true,
+//         scrub: 1,
+//     },
+// });
 
-tl4.to(".c-one", {
-    marginTop: "-39%",
-    opacity: "1",
-}, 'sct-1')
+// const tl4 = gsap.timeline({ paused: true });
 
-tl4.to(".c-two", {
-    opacity: "1",
-}, 'sct-2')
-tl4.to(".c-one", {
-    marginTop: "-78%",
-    opacity: "0",
-}, 'sct-2')
+// tl4.to(".c-one", {
+//     marginTop: "-39%",
+//     opacity: "1",
+// }, 'sct-1')
+// tl4.pause();
 
-tl4.to(".c-three", {
-    opacity: "1",
-}, 'sct-3')
-tl4.to(".c-two", {
-    opacity: "0",
-}, 'sct-3')
-tl4.to(".c-one", {
-    marginTop: "-120%",
-}, 'sct-3')
+// tl4.to(".c-two", {
+//     opacity: "1",
+// }, 'sct-2')
+// tl4.to(".c-one", {
+//     marginTop: "-78%",
+//     opacity: "0",
+// }, 'sct-2')
+// tl4.pause();
 
-
-tl4.to(".c-three", {
-    opacity: "0",
-}, 'sct-4')
-tl4.to(".c-four", {
-    opacity: "1",
-}, 'sct-4')
-tl4.to(".c-one", {
-    marginTop: "-160%",
-}, 'sct-4')
+// tl4.to(".c-three", {
+//     opacity: "1",
+// }, 'sct-3')
+// tl4.to(".c-two", {
+//     opacity: "0",
+// }, 'sct-3')
+// tl4.to(".c-one", {
+//     marginTop: "-120%",
+// }, 'sct-3')
+// tl4.pause();
 
 
-tl4.to(".c-five", {
-    opacity: "1",
-}, 'sct-5')
-tl4.to(".c-four", {
-    opacity: "0",
-}, 'sct-5')
-tl4.to(".c-one", {
-    marginTop: "-200%",
-}, 'sct-5')
+// tl4.to(".c-three", {
+//     opacity: "0",
+// }, 'sct-4')
+// tl4.to(".c-four", {
+//     opacity: "1",
+// }, 'sct-4')
+// tl4.to(".c-one", {
+//     marginTop: "-160%",
+// }, 'sct-4')
 
 
-tl4.to(".c-six", {
-    opacity: "1",
-}, 'sct-6')
-tl4.to(".c-five", {
-    opacity: "0",
-}, 'sct-6')
-tl4.to(".c-one", {
-    marginTop: "-240%",
-}, 'sct-6')
+// tl4.to(".c-five", {
+//     opacity: "1",
+// }, 'sct-5')
+// tl4.to(".c-four", {
+//     opacity: "0",
+// }, 'sct-5')
+// tl4.to(".c-one", {
+//     marginTop: "-200%",
+// }, 'sct-5')
 
 
-tl4.to(".c-seven", {
-    opacity: "1",
-}, 'sct-7')
-tl4.to(".c-six", {
-    opacity: "0",
-}, 'sct-7')
-tl4.to(".c-one", {
-    marginTop: "-280%",
-}, 'sct-7')
+// tl4.to(".c-six", {
+//     opacity: "1",
+// }, 'sct-6')
+// tl4.to(".c-five", {
+//     opacity: "0",
+// }, 'sct-6')
+// tl4.to(".c-one", {
+//     marginTop: "-240%",
+// }, 'sct-6')
+
+
+// tl4.to(".c-seven", {
+//     opacity: "1",
+// }, 'sct-7')
+// tl4.to(".c-six", {
+//     opacity: "0",
+// }, 'sct-7')
+// tl4.to(".c-one", {
+//     marginTop: "-280%",
+// }, 'sct-7')
 
 // tl4.to(".c-seven", {
 //     opacity: "0",
@@ -107,6 +112,50 @@ tl4.to(".c-one", {
 
 
 const gridItems = [...document.querySelectorAll('.grid > .grid__item')];
+
+// document.getElementById("textcnt").addEventListener('click', function () {
+//     document.getElementById("textcnt").style.marginTop = "-100%";
+//     document.getElementById("textcnt").style.marginTop = '-' + document.getElementById("textcnt").style.marginTop + '%'; 
+// })
+
+
+var clickCount = 0;
+function changeText() {
+    
+    if(clickCount == 0) {
+        document.getElementById("textcnt").style.marginTop = '0px';
+        clickCount++;
+    }
+    else if(clickCount == 1) {
+        document.getElementById("textcnt").style.marginTop = '-430px';
+        clickCount++;
+    }
+    else if(clickCount == 2) {
+        document.getElementById("textcnt").style.marginTop = '-860px';
+        clickCount++;
+    }
+    else if(clickCount == 3) {
+        document.getElementById("textcnt").style.marginTop = '-1290px';
+        clickCount++;
+    }
+    else if(clickCount == 4) {
+        document.getElementById("textcnt").style.marginTop = '-1720px';
+        clickCount++;
+    }
+    else if(clickCount == 5) {
+        document.getElementById("textcnt").style.marginTop = '-2150px';
+        clickCount++;
+    }
+    else if(clickCount == 6) {
+        document.getElementById("textcnt").style.marginTop = '-2580px';
+        clickCount++;
+    }
+    // else {
+    //     document.getElementById("textcnt").style.marginTop = '0px';
+    //     clickCount = 0;
+    // }
+    
+}
 
 
 // const lenis = new Lenis({
